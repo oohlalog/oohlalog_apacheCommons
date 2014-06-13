194b5e4ea23e5444c2c1ea534de66c97a15056eb
JulianGriggs <jgriggs@princeton.edu>
2014-06-13 15:19:04 -0700

    Added TODO in TestLogger

--- Apache_Commons_Adapter/src/com/oohlalog/commons/TestLogger.java
+++ Apache_Commons_Adapter/src/com/oohlalog/commons/TestLogger.java
@@ -3,20 +3,23 @@ package com.oohlalog.commons;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+
+
+/**
+ * TODO: Make this a legit test.
+ *
+ */
 public class TestLogger {
 
 	public static void main(String[] args) throws InterruptedException {
 		Log m_log = LogFactory.getLog(TestLogger.class);
 		System.out.println("---Beginning---");
 		for (int i = 0; i < 151; i++) {
-//			m_log.warn("i"+String.valueOf(i), new Throwable("throwable"));
-			m_log.info("i"+String.valueOf(i));
+			m_log.warn(String.valueOf(i), new Throwable("throwable"));
 		}
-		Thread.sleep(2000);
 		for (int i = 151; i < 352; i++) {
-			m_log.error("z"+String.valueOf(i));
+			m_log.error(String.valueOf(i));
 		}
 		System.out.println("end");
-		m_log = null;
 	}
 }
