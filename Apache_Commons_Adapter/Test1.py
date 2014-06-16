bc73ac153f6fffee0e6a82158fc1b195a9049609
JulianGriggs <jgriggs@princeton.edu>
2014-06-16 15:45:55 -0700

    Fixing test 1

--- Apache_Commons_Adapter/Test1.py
+++ Apache_Commons_Adapter/Test1.py
@@ -3,10 +3,11 @@ def main():
 	output = ""
 	for line in sys.stdin:
 		output = output + line
-	if output.count('Received: {"insertCount":150,"success":true}') < 12:
-		print "Failed: Not enough successful posts to OLL"
-	elif output.count('Received: {"insertCount":150,"success":true}') > 12:
-		print "Failed: Too many successful posts to OLL"
-	print output.count('Received: {"insertCount":150,"success":true}')
+	if output.count('Received: {"insertCount":150,"success":true}') < 8:
+		print "FAILURE: Not enough successful posts to OLL"
+	elif output.count('Received: {"insertCount":150,"success":true}') > 8:
+		print "FAILURE: Too many successful posts to OLL"
+	else:
+		print "SUCCESS"
 
 main()
